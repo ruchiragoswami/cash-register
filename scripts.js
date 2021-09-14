df72b4ca0584d45db4c9450b82bffd8927dac1cb
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-14 08:16:04 -0700

    trying code
    Kept trying to reach the goal

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const billAmount = document.querySelector("#bill-amount"); 
+const see = document.querySelector("#btn-see ");
+
+
+
+// see.addEventListener("click", check);
+
+
+
+// console.log("billAmount.value");
+console.log(billAmount.value); 
+
