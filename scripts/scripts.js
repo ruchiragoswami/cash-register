29d29f40a72f81d061d5e9481327d430baae5284
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-14 10:47:38 -0700

    js file updted
    Added more code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,13 @@ let givenCash = document.querySelector("#given-cash");
 let btnValid = document.querySelector("#btn-valid");
 let btnCalc = document.querySelector("#btn-calc");
 let btnReset = document.querySelector("#btn-reset");
-let showError = document.querySelector("#show-error"); 
+let showMsg = document.querySelector("#show-message"); 
 let lowerDiv = document.querySelector("#lower"); 
 let table = document.querySelector("TABLE");
 
+let ourNotes = [2000, 500, 100, 20, 10, 5, 1]
 
-
-showError.style.display="none"; 
+showMsg.style.display="none"; 
 lowerDiv.style.display="none";
 table.style.display="none";
 
@@ -18,23 +18,37 @@ table.style.display="none";
 
 function validate() {
    if (billAmount.value <=0) {
-      showError.innerText = "Please enter a valid amount!"; 
-      showError.style.display="block";
+      showMsg
+      .innerText = "Please enter a valid amount!"; 
+      showMsg
+      .style.display="block";
    } else {
       lowerDiv.style.display="block";
       btnValid.style.display="none"; 
-      showError.style.display="none"; 
+      showMsg.style.display="none"; 
+   }
+}
+
+function actualCount(someNum) {
+   let num=  Math.trunc(someNum) ; 
+   for (let i=0; i < ourNotes.length; i++) {
+      ourNotes[i]/num 
    }
 }
 
 
 function calculate() {
    if (billAmount.value > givenCash.value) {
-      showError.innerText = "Given cash cannot be lesser than the bill amount!";
-      showError.style.display="block"; 
+      showMsg
+      .innerText = "Given cash cannot be lesser than the bill amount!";
+      showMsg
+      .style.display="block"; 
    } else {
-      showError.style.display="none"; 
+      let difference = givenCash.value - billAmount.value; 
+      showMsg.innerText=  difference + " is to be returned"
+      showMsg.style.display="block"; 
       table.style.display="block"; 
+      
    }
 }
 
@@ -45,7 +59,7 @@ btnValid.addEventListener("click", validate);
 btnCalc.addEventListener("click", calculate); 
 
 // function reset() {
-//    showError.style.display="none";
+//    showMsg.style.display="none";
 //    lowerDiv.style.dispaly="none";
 //    table.style.display="none"; 
 //    btnValid.style.display="inline"; 
